ec5dbc121fd24a8883c509457d8ac1ff81ca7c11
Rubikal23 <Rubikal23@users.noreply.github.com>
2016-04-15 00:58:06 +0530

    Added files via upload

--- HashMap.java
+++ HashMap.java
@@ -1,16 +1,44 @@
+import java.util.ArrayList;
 
-public class HashMap<T> {
+@SuppressWarnings("hiding")
+public class HashMap<Object> {
 	
-	private T key;
-	private T value;
+	private ArrayList<Object> key;
+	private ArrayList<Object>value;
+//	private static int index = -1;
 	
-	
-	public HashMap(T key, T value)
+	public HashMap()
 	{
-		this.key = key;
-		this.value = value;
+		key =  new ArrayList<Object>();
+		value = new ArrayList<Object>();
 	}
 	
+	public Object get(Object key)
+	{
+		int i = this.key.indexOf(key);
+	
+		if(i != -1)
+			return this.value.get(i);
+			
+		return null;
+	}
+	
+	public void put(Object key, Object value)
+	{
+		this.key.add(key);
+		this.value.add(value);
+	}
+	
+	public Object remove(Object key)
+	{
+		int i = this.key.indexOf(key);
+		if(i != -1)
+		{
+			this.key.remove(i);
+			return this.value.remove(i);
+		}
+		return null;
+	}
 	
 
 }
--- Main.java
+++ Main.java
@@ -1,9 +1,18 @@
-
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+		HashMap<Object> h = new HashMap<Object>();
+		
+		h.put(1, "One");
+		h.put(2, "Two");
+		h.put(3, "Three");
+		h.put(4, "Four");
+		
+		System.out.println(h.get(2));
+		System.out.println(h.remove(3));
+		System.out.println(h.get(4));
 	}
 
 }
